8a29d57c23e814c1f2b3ef83552b2d84f7fd747a
TrungRegit <trungnv@regit-technology.com>
2023-11-23 06:59:13 +0700

    formatter code

--- js/home.js
+++ js/home.js
@@ -1,13 +1,13 @@
 let header = document.querySelector("#headerScroll");
 let previousScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-window.addEventListener('scroll', function() {
-  let currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-  if (currentScrollPosition > previousScrollPosition) {
-    header.classList.remove('backInDown');
+window.addEventListener('scroll', function () {
+    let currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScrollPosition > previousScrollPosition) {
+        header.classList.remove('backInDown');
         header.classList.add('backOutUp');
-  } else if (currentScrollPosition < previousScrollPosition) {
-    header.classList.remove('backOutUp');
-    header.classList.add('backInDown');
-  }
-  previousScrollPosition = currentScrollPosition;
+    } else if (currentScrollPosition < previousScrollPosition) {
+        header.classList.remove('backOutUp');
+        header.classList.add('backInDown');
+    }
+    previousScrollPosition = currentScrollPosition;
 });
